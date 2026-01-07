72fa905a561c301148965dde5f17032d3da2e6e6
Douglas <dellonath@gmail.com>
2026-01-07 00:30:24 -0300

    feat/adding new instance

--- mature.py
+++ mature.py
@@ -3,7 +3,7 @@ import string
 import random
 import time
 from dotenv import load_dotenv
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from models.wapi import WAPI
 from models.flow import FlowActionType
 
@@ -16,11 +16,28 @@ class AgentInstance:
     phone: str
     instance_id: int
     instance_token: int
-    block: list[str]
+    block_sender: list[str] = field(default_factory=list)
+
+DELAY_BETWEEN_INTERACTIONS: int = random.randint(3, 20)
+
+ACTIONS = [
+    {
+        'type': FlowActionType.SEND_MESSAGE,
+        'prob': 0.7
+    },
+    {
+        'type': FlowActionType.SEND_AUDIO,
+        'prob': 0.25
+    },
+    {
+        'type': FlowActionType.SEND_IMAGE,
+        'prob': 0.05
+    }
+]
 
 INSTANCES = [
     {
-        "active": False,
+        "active": True,
         "name": "Chip 1",
         "phone": "553597693994",
         "instance_id": "WAPI_INSTANCE_ID_CHIP1",
@@ -40,7 +57,7 @@ INSTANCES = [
         "phone": "553588629435",
         "instance_id": "WAPI_INSTANCE_ID_RMENDES",
         "instance_token": "WAPI_INSTANCE_TOKEN_RMENDES",
-        "block": [
+        "block_sender": [
             "553599748699"
         ]
     },
@@ -64,37 +81,35 @@ INSTANCES = [
         "phone": "553599748699",
         "instance_id": "WAPI_INSTANCE_ID_DOUGLASOLIVEIRA",
         "instance_token": "WAPI_INSTANCE_TOKEN_DOUGLASOLIVEIRA",
-        "block": [
+        "block_sender": [
             "553588629435"
         ]
     }
 ]
 
 AUDIOS = [
-    "https://audio.jukehost.co.uk/gjx488CfOgtEHMHx9CUHnadNiYFq6k47.ogg", # tagarela 4 secs
-    "https://audio.jukehost.co.uk/ZeLPWpF6zKitGd2xHy4AruKxigTfHsji.ogg", # tagarela 5 secs
-    "https://audio.jukehost.co.uk/zij15c6kGSC2cmiJf9BF4FNhoI67bXvA.ogg", # tagarela 6 secs
-    "https://audio.jukehost.co.uk/DrGDkuNtgYoVLXYwQsMGTRXAkDGYhOAq.ogg", # tagarela 7 secs
-    "https://audio.jukehost.co.uk/fYnBgU3tJbcmfVbsjN5xY7YfNHWFbwwJ.ogg", # tagarela 8 secs
-    "https://audio.jukehost.co.uk/6wtE0udkRWEstzmwbAURxyYXcg9Z51LY.ogg", # tagarela 9 secs
-    "https://audio.jukehost.co.uk/kZpZiXbUTTYEH7jSIc01YJfvuIR2uXiK.ogg", # tagarela 10 secs
-    "https://audio.jukehost.co.uk/IC6fOaai6qKy0Nw3kbOisxGNXHsiBRlg.ogg", # tagarela 11 secs
-    "https://audio.jukehost.co.uk/1NDi0hAAqdbcmzZXSTyW5pkA30VvySFd.ogg", # tagarela 12 secs
-    "https://audio.jukehost.co.uk/Orf2GERba9h3BWmaGysAHOvAw3GLsmwt.ogg", # tagarela 13 secs
-    "https://audio.jukehost.co.uk/x28Z9Yb1WVtaWlPqIZI264C9LfOjp1kR.ogg", # tagarela 14 secs
-    "https://audio.jukehost.co.uk/3q0JEqaD7SV9zDrfCfsC4qEzj63qt6HA.ogg", # tagarela 15 secs
-
+    'https://res.cloudinary.com/dg0nvnjqw/video/upload/v1767726360/tagarela_4_secs_clapku.ogg',
+    'https://res.cloudinary.com/dg0nvnjqw/video/upload/v1767726359/tagarela_5_secs_cyhygd.ogg',
+    'https://res.cloudinary.com/dg0nvnjqw/video/upload/v1767726358/tagarela_6_secs_ejuglr.ogg',
+    'https://res.cloudinary.com/dg0nvnjqw/video/upload/v1767726358/tagarela_7_secs_wmakxo.ogg',
+    'https://res.cloudinary.com/dg0nvnjqw/video/upload/v1767726358/tagarela_8_secs_bxyamk.ogg',
+    'https://res.cloudinary.com/dg0nvnjqw/video/upload/v1767726358/tagarela_9_secs_smegzs.ogg',
+    'https://res.cloudinary.com/dg0nvnjqw/video/upload/v1767726358/tagarela_10_secs_lryspe.ogg',
+    'https://res.cloudinary.com/dg0nvnjqw/video/upload/v1767726358/tagarela_11_secs_b1poes.ogg',
+    'https://res.cloudinary.com/dg0nvnjqw/video/upload/v1767726357/tagarela_12_secs_oapknw.ogg',
+    'https://res.cloudinary.com/dg0nvnjqw/video/upload/v1767726357/tagarela_13_secs_ed554e.ogg',
+    'https://res.cloudinary.com/dg0nvnjqw/video/upload/v1767726357/tagarela_14_secs_nxpcxn.ogg',
+    'https://res.cloudinary.com/dg0nvnjqw/video/upload/v1767726218/tagarela_15_secs_bd0qoz.ogg'
 ]
 
 IMAGES = [
-    "https://res.cloudinary.com/dg0nvnjqw/image/upload/v1764680916/main-sample.png",
-    "https://res.cloudinary.com/dg0nvnjqw/image/upload/v1764680916/cld-sample-5.jpg",
-    "https://res.cloudinary.com/dg0nvnjqw/image/upload/v1764680915/cld-sample-4.jpg",
-    "https://res.cloudinary.com/dg0nvnjqw/image/upload/v1764680915/cld-sample-3.jpg",
-    "https://res.cloudinary.com/dg0nvnjqw/image/upload/v1764680914/cld-sample.jpg",
-    "https://res.cloudinary.com/dg0nvnjqw/image/upload/v1764680915/cld-sample-2.jpg",
-    "https://res.cloudinary.com/dg0nvnjqw/image/upload/v1764680914/samples/waves.png",
-
+    'https://res.cloudinary.com/dg0nvnjqw/image/upload/v1764680916/main-sample.png',
+    'https://res.cloudinary.com/dg0nvnjqw/image/upload/v1764680916/cld-sample-5.jpg',
+    'https://res.cloudinary.com/dg0nvnjqw/image/upload/v1764680915/cld-sample-4.jpg',
+    'https://res.cloudinary.com/dg0nvnjqw/image/upload/v1764680915/cld-sample-3.jpg',
+    'https://res.cloudinary.com/dg0nvnjqw/image/upload/v1764680914/cld-sample.jpg',
+    'https://res.cloudinary.com/dg0nvnjqw/image/upload/v1764680915/cld-sample-2.jpg',
+    'https://res.cloudinary.com/dg0nvnjqw/image/upload/v1764680914/samples/waves.png',
 ]
 
 def generate_random_text(chars=string.ascii_lowercase + string.ascii_uppercase + string.digits + '!?-.'):
@@ -109,40 +124,36 @@ for _ in range(300):
 
     active_instances: list[dict] = [instance for instance in INSTANCES if instance.get('active')]
 
-    sender_instance: dict = random.choices(
-        population=active_instances
-    )[0]
+    sender_instance: AgentInstance = AgentInstance(
+        **random.choices(population=active_instances)[0]
+    )
 
-    receiver_instances: list = random.choices(
+    receiver_instances: list[AgentInstance] = random.choices(
         population=[
-            instance for instance in active_instances 
-            if instance.get('phone') != sender_instance.get('phone')
-            and instance.get('phone') not in sender_instance.get('block', [])
+            AgentInstance(**instance) for instance in active_instances 
+            if instance.get('phone') != sender_instance.phone
+            and instance.get('phone') not in sender_instance.block_sender
         ],
         k=random.randint(1, len(active_instances) - 1)
     )
 
     w_api: WAPI = WAPI(
-        instance_id=os.getenv(sender_instance.get('instance_id')),
-        instance_token=os.getenv(sender_instance.get('instance_token')),
+        instance_id=os.getenv(sender_instance.instance_id),
+        instance_token=os.getenv(sender_instance.instance_token),
     )
 
     for receiver_instance in receiver_instances:
         number_of_random_actions: int = random.randint(1, 3)
         print(
             f'Interaction started ' 
-            f'from {sender_instance.get("name")} ({sender_instance.get("phone")}) '
-            f'to {receiver_instance.get("name")} ({receiver_instance.get("phone")}) '
+            f'from {sender_instance.name} ({sender_instance.phone}) '
+            f'to {receiver_instance.name} ({receiver_instance.phone}) '
             f'({number_of_random_actions} actions):'
         )
         for _ in range(number_of_random_actions):
             action: FlowActionType = random.choices(
-                population=[
-                    FlowActionType.SEND_MESSAGE,
-                    FlowActionType.SEND_AUDIO,
-                    FlowActionType.SEND_IMAGE
-                ],
-                weights=[0.6, 0.3, 0.1],
+                population=[action['type'] for action in ACTIONS],
+                weights=[action['prob'] for action in ACTIONS],
             )[0]
 
             seconds: int = random.randint(1, 15)
@@ -152,26 +163,26 @@ for _ in range(300):
 
             if action == FlowActionType.SEND_MESSAGE:
                 request: dict = w_api.send_message(
-                    phone=receiver_instance.get('phone'),
+                    phone=receiver_instance.phone,
                     message=generate_random_text(),
                     delay=delay
                 )
 
             elif action == FlowActionType.SEND_AUDIO:
                 request: dict = w_api.send_audio(
-                    phone=receiver_instance.get('phone'),
+                    phone=receiver_instance.phone,
                     audio_url=random.choice(AUDIOS),
                     delay=delay
                 )
 
             elif action == FlowActionType.SEND_IMAGE:
                 request: dict = w_api.send_image(
-                    phone=receiver_instance.get('phone'),
+                    phone=receiver_instance.phone,
                     image_url=random.choice(IMAGES),
                     delay=delay
                 )
             
             time.sleep(seconds)
 
-    time.sleep(random.randint(5, 20))
+    time.sleep(DELAY_BETWEEN_INTERACTIONS)
     
